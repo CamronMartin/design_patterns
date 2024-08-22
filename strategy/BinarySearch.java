0ebd85934211951b35f020dd6a9fc49891a4e2e8
PowerOfLegends <alrightybros@gmail.com>
2024-08-22 15:23:52 -0400

    finalized the binary search class

--- strategy/BinarySearch.java
+++ strategy/BinarySearch.java
@@ -12,9 +12,22 @@ public class BinarySearch implements SearchBehavior{
     public boolean contains(List<Person> people, Person person) {
 
         Collections.sort(people);
-        int key = people.size()/2;
+        lowerBound = 0;
+        upperBound = people.size() - 1;
+
+        while(lowerBound <= upperBound) {
+
+            middleBound = (lowerBound + upperBound) / 2;
+            if(person.compareTo(people.get(middleBound)) == 0) {
+                return true;
+            } else if(person.compareTo(people.get(middleBound)) > 0) {
+                lowerBound = middleBound + 1;
+            } else {
+                upperBound = middleBound - 1;
+            }
+        }// Ending bracket of While loop
         return false;
-        
+
     }// Ending bracket of method contains()
     
 }// Ending bracket of class BinarySearch
