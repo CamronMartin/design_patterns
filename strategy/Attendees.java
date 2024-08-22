0a26338ace69391b14d917884e8ea7ab2a25d572
PowerOfLegends <alrightybros@gmail.com>
2024-08-22 14:29:44 -0400

    implemented attendees class

--- strategy/Attendees.java
+++ strategy/Attendees.java
@@ -8,6 +8,23 @@ public class Attendees {
     private List<Person> people;
     private SearchBehavior searchBehavior;
 
-    
-    
+    public Attendees(String title) {
+        this.title = title;
+    }// Ending bracket of constructor
+
+    public Person add(String firstName, String lastName, String phoneNumber, String reminder) {
+        return new Person(firstName, lastName, phoneNumber, reminder);
+    }// Ending bracket of method add()
+
+    public String getTitle() {
+        return this.title;
+    }// Ending bracket of method getTitle()
+
+    public void setSearchBehavior(SearchBehavior searchBehavior) {
+        this.searchBehavior = searchBehavior;
+    }// Ending bracket of method setSearchBehavior
+
+    public List<Person> getList() {
+        return this.people;
+    }// Ending bracket of method getList
 }
--- strategy/BinarySearch.java
+++ strategy/BinarySearch.java
@@ -5,10 +5,16 @@ import java.util.List;
 
 public class BinarySearch implements SearchBehavior{
 
+    int lowerBound;
+    int middleBound;
+    int upperBound;
+
     public boolean contains(List<Person> people, Person person) {
+
         Collections.sort(people);
         int key = people.size()/2;
         return false;
+        
     }// Ending bracket of method contains()
     
 }// Ending bracket of class BinarySearch
