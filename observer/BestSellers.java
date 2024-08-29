2c72d9e593922d9122ef44083dae329f6426b1ab
CamronMartin <cbrycem02@gmail.com>
2024-08-28 21:06:47 -0400

    Added methods to each class before using observer.

--- observer/BestSellers.java
+++ observer/BestSellers.java
@@ -1,5 +1,24 @@
 package observer;
 
+import java.util.ArrayList;
+
 public class BestSellers {
+    
+    private ArrayList<Book> bestSellers = new ArrayList<>();
+
+    
+
+    /**
+     * This will add a book to the best sellers collection.
+     * @param title of the book
+     * @param genre of the book 
+     * @param authorFirstname of the book
+     * @param authorLastName of the book
+     * @param description of the book
+     */
+    public void addBook(String title, Genre genre, String authorFirstname, String authorLastName, String description) {
+        Book b = new Book(title, genre, authorFirstname, authorLastName, description);
+        bestSellers.add(b);
+    }
     
 }
--- observer/Store.java
+++ observer/Store.java
@@ -1,5 +1,29 @@
 package observer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * This class will be an object representation of a Book-Store
+ * @author Camron Martin
+ */
 public class Store {
+
+    private Subject subject;
+    private String title;
+    private final Queue<Book> bestSellers;
+
+    public Store(Subject subject) {
+        this.subject = subject;
+        this.bestSellers = new LinkedList<>();
+    }
+
+    public void update(Book book) {
+        // TODO: Implement this method
+    }
+
+    public LinkedList<Book> getBestSellers() {
+        return (LinkedList<Book>) this.bestSellers;
+    }
     
 }
