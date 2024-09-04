d41b0c6d82e2b751cbb44890fd01bed4f61b6d91
PowerOfLegends <alrightybros@gmail.com>
2024-09-04 17:35:28 -0400

    Added class files, and implemented Character class.

---
+++ decorator/Character.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+/**
+ * This class will be used to instantiate a character
+ * @author Camron Martin
+ */
+public class Character {
+
+    private String name;
+    protected ArrayList<String> lines;
+
+
+    /**
+     * This method will return the name of 
+     * the created character
+     * @return name of character
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        // TODO: Finish
+        String rv = "";
+        return rv;
+    }
+    
+}
