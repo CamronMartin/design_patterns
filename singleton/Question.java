281a259199a36e7a027405838d36d982b7d684b4
CamronMartin <cbrycem02@gmail.com>
2024-09-18 18:50:38 -0400

    Added necessary java doc comments.

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,12 +1,24 @@
 package singleton;
 import java.util.ArrayList;
 
+/**
+ * This class will be responsible for creating a Question and checking
+ * whether the user inputs the correct answers.
+ * @author Camron Martin
+ */
 public class Question {
 
   private String question;
   private ArrayList<String> answers;
   private int correctAnswer;
 
+  /**
+   * This constructor will create a question with a list of answers 
+   * to choose from. 
+   * @param question A question to ask.
+   * @param correctAnswer The correct answer to the questions.
+   * @param answers An array of answers to choose from.
+   */
   public Question(String question, int correctAnswer, String[] answers) {
     this.question = question;
     this.correctAnswer = correctAnswer; 
@@ -18,6 +30,11 @@ public class Question {
     
   }
 
+  /**
+   * This will print out the question along with 
+   * the four answers to choose from.
+   * @return String question and answers 
+   */
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append(this.question).append("\n");
@@ -27,10 +44,19 @@ public class Question {
     return sb.toString();
   }
 
+  /**
+   * This method will check wether the user 
+   * got the correct answer or not 
+   * @return boolean correct or incorrect
+   */
   public boolean isCorrect(int userAnswer) {
     return userAnswer == correctAnswer + 1;
   }
 
+  /**
+   * This will output the correct answer
+   * @return String correct answer 
+   */
   public String getCorrectAnswer() {
     StringBuilder sb = new StringBuilder();
     sb.append(this.correctAnswer + 1);
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -3,6 +3,11 @@ package singleton;
 import java.util.Random;
 import java.util.ArrayList;
 
+/**
+ * This class will be responsible for handling everything for the triviaGame 
+ * so it will only be instansiated once. 
+ * @author Camron Martin
+ */
 public class TriviaGame {
   
   private static TriviaGame triviaGame;
@@ -11,8 +16,20 @@ public class TriviaGame {
   private Random rand;
   private ArrayList<Question> questions;
 
-  private TriviaGame() {}
+  /**
+   * This is a private constructor and will not be called, it will only 
+   * initialize the rand variable and questions ArrayList
+   */
+  private TriviaGame() {
+    this.questions = new ArrayList<>(DataLoader.getTriviaQuestions());
+    rand = new Random();
+  }
 
+  /**
+   * This method will be responsible for instansiating the TriviaGame 
+   * instead of using the Constructor
+   * @return TriviaGame
+   */
   public static TriviaGame getInstance() {
     if (triviaGame == null) {
       System.out.println("Creating new TriviaGame");
@@ -21,33 +38,54 @@ public class TriviaGame {
     return triviaGame;
   }
 
+  /**
+   * This method will print a question and then remove it so it 
+   * will not be asked again 
+   * @return question Question to be asked 
+   */
   public Question getQuestion() {
-
-    // TODO: Implement way for questions to not repeat
-    this.questions = new ArrayList<>();
-    for(int i = 0; i < DataLoader.getTriviaQuestions().size(); ++i) {
-      this.questions.add(DataLoader.getTriviaQuestions().get(i));
+    if(!hasMoreQuestions()) {
+      System.out.println("No more questions available.");
     }
-    rand = new Random();
+    numGames++;
     int pos = rand.nextInt(questions.size());
-    return this.questions.get(pos);
+    return this.questions.remove(pos);
   }
 
+
+  /**
+   * This method will check whether the question array list 
+   * is empty or not 
+   * @return boolean empty or not
+   */
   public boolean hasMoreQuestions() {
-    return questions.size() > 0;
+    return this.questions.size() > 0;
   }
 
+  /**
+   * this will inrease the score if they 
+   * get the answer correct 
+   */
   public void winRound() {
     this.score++;
   }
 
+  /**
+   * This will decrease the score if they 
+   * get the answer inccorect 
+   */
   public void loseRound() {
     this.score--;
   }
 
+  /**
+   * This will return the amount of answers they 
+   * got correct out of however many questions 
+   * has been asked. 
+   */
   public String getScores() {
     StringBuilder sb = new StringBuilder();
-    sb.append(this.score);
+    sb.append("You scored " + (this.score + 1) + "/" + numGames);
     return sb.toString();
   }
 
