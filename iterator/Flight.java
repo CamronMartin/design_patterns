7db3df88a9d219fe8b9a9f9b938a38e1559789d1
CamronMartin <cbrycem02@gmail.com>
2024-09-30 18:48:55 -0400

    Added necessary java doc comments.

--- iterator/Flight.java
+++ iterator/Flight.java
@@ -2,6 +2,11 @@ package iterator;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
+/**
+ * This will class will create a new flight and print 
+ * a string representation of a flight. 
+ * @author Camron Martin 
+ */
 public class Flight {
 
   private String flightNum;
@@ -11,6 +16,16 @@ public class Flight {
   private LocalTime endTime;
   private int numTransfers;
 
+  /**
+   * This constructor will create a new flight with times, 
+   * location, and number of transfers 
+   * @param flightNum number of flight 
+   * @param from where you are coming from 
+   * @param to where you are going to 
+   * @param startTime start time for the flight 
+   * @param endTime end time for the flight 
+   * @param numTransfers numbers of transfers for entire flight 
+   */
   public Flight(String flightNum, Airport from, Airport to, LocalTime startTime, LocalTime endTime, int numTransfers) {
     this.flightNum = flightNum;
     this.from = from;
@@ -21,18 +36,36 @@ public class Flight {
    
   }
 
+  /**
+   * Method to return where you are coming from 
+   * @return from location 
+   */
   public Airport getFrom() {
     return this.from;
   }
 
+  /**
+   * Method to return where you are going to 
+   * @return destination  
+   */
   public Airport getTo() {
     return this.to;
   }
 
+  /**
+   * Method to check whether or not the from and to locations 
+   * are the same 
+   * @return boolean of whether or not they are the same
+   */ 
   public boolean sameLoc(Airport from, Airport to) {
     return this.from.equals(from) && this.from.equals(to);
   }
 
+  /**
+   * This method will print a string representation 
+   * of the flight list according to the flight loader 
+   * @return string representation of available flights
+   */
   @Override
   public String toString() {
 
--- iterator/FlightIterator.java
+++ iterator/FlightIterator.java
@@ -3,6 +3,12 @@ package iterator;
 import java.util.Iterator;
 import java.util.ArrayList;
 
+/**
+ * This class will be responsible for checking whether there is 
+ * a next flight or not, and if there is, it will tell you 
+ * the next flight.
+ * @author Camron Martin
+ */
 public class FlightIterator implements Iterator {
 
   private ArrayList<Flight> flights;   
@@ -10,11 +16,22 @@ public class FlightIterator implements Iterator {
   private Airport from;
   private Airport to;
 
+  /**
+   * Constructor that will create the flight iterator.
+   * @param flights a list of available flights 
+   * @param from starting location 
+   * @param to ending location
+   */
   public FlightIterator(ArrayList<Flight> flights, Airport from, Airport to){
     this.flights = flights;
     this.from = from;
     this.to = to;
   }
+
+  /**
+   * Method to check whether there is a next item in the list 
+   * @return boolean if there is a next item.
+   */
   public boolean hasNext() { 
 
     while (position < flights.size()) {
@@ -27,6 +44,11 @@ public class FlightIterator implements Iterator {
     return false;
 
   }
+
+  /**
+   * Method that will return the next flight in the list.
+   * @return flight the next flight.
+   */
   public Flight next() {
 
     if(!hasNext()){
